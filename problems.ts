a2f584859f0b9cf8adbc7546310486effc2fc674
Hafsa-Begum <86627326+Hafsa-Begum@users.noreply.github.com>
2025-05-07 23:14:33 +0600

    solve problem-2

--- problems.ts
+++ problems.ts
@@ -9,6 +9,24 @@ function formatString(input: string, toUpper?: boolean): string{
     }
 }
 
-formatString("Hello");          // Output: "HELLO"
-formatString("Hello", true);   // Output: "HELLO"
-formatString("Hello", false);  // Output: "hello"
+// formatString("Hello");          // Output: "HELLO"
+// formatString("Hello", true);   // Output: "HELLO"
+// formatString("Hello", false);  // Output: "hello"
+
+function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[]{
+    const filteredData = items.filter(item => item.rating >= 4  );
+    console.log(filteredData);
+    return filteredData;
+}
+
+// const books = [
+//     { title: "Book A", rating: 4.5 },
+//     { title: "Book B", rating: 3.2 },
+//     { title: "Book C", rating: 5.0 }
+//   ];
+  
+// filterByRating(books); 
+
+function concatenateArrays<T>(...arrays: T[][]): T[]{
+
+}
