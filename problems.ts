fe003be2f3eaf0dec0d1d975fc38e092fe14553e
Hafsa-Begum <86627326+Hafsa-Begum@users.noreply.github.com>
2025-05-08 19:13:00 +0600

    solve all problems

--- problems.ts
+++ problems.ts
@@ -28,5 +28,119 @@ function filterByRating(items: { title: string; rating: number }[]): { title: st
 // filterByRating(books); 
 
 function concatenateArrays<T>(...arrays: T[][]): T[]{
-
+    console.log('arrays', [...arrays])
+    let concated: T[]  = [];
+    
+    arrays.map(a=> concated.push(...a));
+    console.log('concate', concated)
+    return concated;
 }
+concatenateArrays(["a", "b"], ["c"]);       // Output: ["a", "b", "c"]
+concatenateArrays([1, 2], [3, 4], [5]); 
+
+class Vehicle {
+    private make: string;
+    private year: number;
+    constructor (make: string, year: number){
+        this.make = make;
+        this.year = year;
+    }
+    getInfo(){
+        console.log(`"Make: ${this.make}, Year: ${this.year}"`)
+    }
+}
+
+class Car extends Vehicle{
+    private model: string;
+
+    constructor(make: string, year: number, model: string){
+        super(make, year)
+        this.model = model;
+    }
+    getModel(){
+        console.log(`"Model: ${this.model}"`)
+    }
+}
+
+const myCar = new Car("Toyota", 2020, "Corolla");
+myCar.getInfo();
+myCar.getModel();
+
+
+function processValue(value: string | number): number{
+    if(typeof(value) === 'string'){
+        console.log('length', value.length);
+        return value.length;
+    }
+    else{
+        console.log('double', value * 2);
+        return value * 2;
+    }
+}
+processValue("hello"); // Output: 5
+processValue("10");      // Output: 20
+
+interface Product {
+    name: string;
+    price: number;
+  }
+  
+  function getMostExpensiveProduct(products: Product[]): Product | null{
+    if(products.length === 0){
+        return null;
+    }
+    else{
+        let highest:Product = products.reduce((max, obj) => obj.price > max.price ? obj : max, products[0]);
+        console.log('highest', highest);
+        return highest;
+    }
+  }
+
+  const products = [
+    { name: "Pen", price: 10 },
+    { name: "Notebook", price: 25 },
+    { name: "Bag", price: 50 }
+  ];
+  
+  getMostExpensiveProduct(products);
+
+
+  enum Day {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+  }
+  
+  function getDayType(day: Day): string{
+    if(day === Day.Saturday || day === Day.Sunday){
+        console.log("Weekend")
+        return "Weekend";
+    }
+    else{
+        console.log("Weekday")
+        return "Weekday";
+    }
+  }
+
+getDayType(Day.Monday);   // Output: "Weekday"
+getDayType(Day.Sunday);   // Output: "Weekend"
+
+async function squareAsync(n: number): Promise<number>{
+    return new Promise((resolve, reject)=>{
+        if(n>=0){
+            setTimeout(()=>{
+                resolve(n * n);
+            }, 1000)
+        }
+        else{
+            reject("Error: Negative number not allowed")
+        }
+    })
+}
+
+squareAsync(4).then(console.log);        // Output after 1s: 16
+squareAsync(-3).catch(console.error);
